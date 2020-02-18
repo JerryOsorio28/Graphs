8cf550b43ecdd09df6cd12898ca71c835878a76a
JerryOsorio28 <jerryOsorio28@users.noreply.github.com>
2020-02-18 16:49:08 -0500

    initialized graph in ancestor.py

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,47 @@
+class Graph:
+    def __init__(self):
+        self.vertices = {}
+    
+    def add_vertex(self, vertex_id):
+        if vertex_id in self.vertices:
+            print("WARNING: That vertex already exists")
+        else:
+            self.vertices[vertex_id] = set()
 
-def earliest_ancestor(ancestors, starting_node):
-    pass
+    def add_edge(self, v1, v2):
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else:
+            raise IndexError("That vertex does not exist!")
+
+    def get_neighbors(self, vertex_id):
+        return self.vertices[vertex_id]
+
+    def earliest_ancestor(self, ancestors, starting_node):
+        print('ancestors', ancestors)
+        print('starting_node', starting_node)
+
+if __name__=='__main__':
+    graph = Graph()
+    graph.add_vertex(1)
+    graph.add_vertex(2)
+    graph.add_vertex(3)
+    graph.add_vertex(4)
+    graph.add_vertex(5)
+    graph.add_vertex(6)
+    graph.add_vertex(7)
+    graph.add_vertex(8)
+    graph.add_vertex(9)
+    graph.add_vertex(10)
+    graph.add_vertex(11)
+    graph.add_edge(10, 1)
+    graph.add_edge(1, 3)
+    graph.add_edge(2, 3)
+    graph.add_edge(3, 6)
+    graph.add_edge(5, 6)
+    graph.add_edge(5, 7)
+    graph.add_edge(4, 5)
+    graph.add_edge(4, 8)
+    graph.add_edge(11, 8)
+    graph.add_edge(8, 9)
+    print('graph', graph.vertices)
