649c2ec767a3df1f57a984a6c1b10ae8110da66f
JerryOsorio28 <jerryOsorio28@users.noreply.github.com>
2020-02-20 18:54:59 -0500

    working on the adventure labyrinth, having fun

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -1,6 +1,7 @@
 from room import Room
 from player import Player
 from world import World
+from util import Queue
 
 import random
 from ast import literal_eval
@@ -11,10 +12,10 @@ world = World()
 
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
-# map_file = "maps/test_cross.txt"
+map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-map_file = "maps/main_maze.txt"
+# map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
 room_graph=literal_eval(open(map_file, "r").read())
@@ -26,15 +27,67 @@ world.print_rooms()
 player = Player(world.starting_room)
 
 # Fill this out with directions to walk
-# traversal_path = ['n', 'n']
 traversal_path = []
-
-
 
 # TRAVERSAL TEST
 visited_rooms = set()
 player.current_room = world.starting_room
 visited_rooms.add(player.current_room)
+
+def explore_lab(trv_path):
+
+    visited = set()
+    player_location_history = []
+    
+    counter = 0
+
+    # Create an empty queue
+    queue = Queue()
+    # Add the initial vertex to the path
+    player_location_history.append(player.current_room)
+    # print('path', path)
+    # Then add the path to the queue, should be initialized as a empty list
+    queue.enqueue(player_location_history)
+    # # While the queue is not empty...
+    while queue.size() > 0:
+        # Dequeue, the first PATH
+        player_location_history = queue.dequeue() 
+        # GRAB THE LAST VERTEX FROM THE PATH
+        last_room = player_location_history[-1]
+        # else check if it's been visited
+        if last_room not in visited:
+        # If it has not been visited...
+            # Added to the visited dic
+            visited.add(last_room)
+            # iterate over the last vertex's neighbors..
+            if player.current_room.n_to is not None:
+                player.current_room = player.current_room.n_to
+                trv_path.append('n')
+                counter += 1
+            if player.current_room not in visited:
+                # if it is not, we make a copy of the path
+                copy_location_history = player_location_history.copy()
+                # we append the neighbor to the copy of the path
+                copy_location_history.append(player.current_room)
+                # then we add the copy of the path to the queue
+                queue.enqueue(copy_location_history)
+            for i in range(counter):
+                print('i', i)
+                player.current_room = player.current_room.s_to
+            print('visited', visited)
+            counter = 0
+            queue.enqueue(player.current_room)
+        if player.current_room.w_to is not None:
+            player.current_room = player.current_room.w_to
+            trv_path.append('w')
+            counter
+    # print('counter', counter)
+    # print('current room', player.current_room)
+            
+explore_lab(traversal_path)
+
+# print('traversal_path',traversal_path)
+
 
 for move in traversal_path:
     player.travel(move)
@@ -51,12 +104,12 @@ else:
 #######
 # UNCOMMENT TO WALK AROUND
 #######
-player.current_room.print_room_description(player)
-while True:
-    cmds = input("-> ").lower().split(" ")
-    if cmds[0] in ["n", "s", "e", "w"]:
-        player.travel(cmds[0], True)
-    elif cmds[0] == "q":
-        break
-    else:
-        print("I did not understand that command.")
+# player.current_room.print_room_description(player)
+# while True:
+#     cmds = input("-> ").lower().split(" ")
+#     if cmds[0] in ["n", "s", "e", "w"]:
+#         player.travel(cmds[0], True)
+#     elif cmds[0] == "q":
+#         break
+#     else:
+#         print("I did not understand that command.")
